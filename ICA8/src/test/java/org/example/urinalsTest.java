d6775f3aa27e750c74deecf1c4df237cc32f6666
blenkala <blenkala@asu.edu>
2022-10-27 15:00:26 -0700

    created a skeleton

---
+++ ICA8/src/test/java/org/example/urinalsTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @Test
+    void main() {
+        fail("tty");
+    }
+
+    @Test
+    void add() {
+    }
+}
