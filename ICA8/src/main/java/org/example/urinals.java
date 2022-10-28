2737a17145ff1ca0acbba474199781558b672d96
blenkala <blenkala@asu.edu>
2022-10-27 21:16:22 -0700

    committed Test case 2

--- ICA8/src/main/java/org/example/urinals.java
+++ ICA8/src/main/java/org/example/urinals.java
@@ -9,6 +9,19 @@ public class urinals {
 
     }
     public static boolean goodString(String str){
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)=='0' || str.charAt(i)=='1')
+            {
+                if(i>0 && str.charAt(i)=='1' && str.charAt(i-1)=='1')
+                {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
         return true;
     }
 }
--- ICA8/src/test/java/org/example/urinalsTest.java
+++ ICA8/src/test/java/org/example/urinalsTest.java
@@ -6,9 +6,14 @@ import org.junit.platform.engine.support.descriptor.UriSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class urinalsTest {
-
     @Test
-    public void test1GoodString() {
+    public void test3GoodString() {
+        System.out.println("======Bhavani Lenkala == TEST THREE EXECUTED =======");
+        urinals uobj=new urinals();
+        assertEquals(true,uobj.goodString("1001"));
+    }
+    @Test
+    public void test2GoodString() {
       System.out.println("======Bhavani Lenkala == TEST TWO EXECUTED =======");
       urinals uobj=new urinals();
       assertEquals(true,uobj.goodString("10001"));
